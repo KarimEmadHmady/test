385f71dc5b05015d34272df2981372c68e2307aa
Web developer <94483684+KarimEmadHmady@users.noreply.github.com>
2023-08-24 21:23:29 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,68 @@
+let productTitle = document.getElementById('product-title');
+let productDescription = document.getElementById('product-description');
+let productImage1 = document.getElementById('product-image-1');
+let productImage2 = document.getElementById('product-image-2');
+let container = document.querySelector('.container');
+let icons = document.querySelectorAll('.icons img');
 
+let clickIndex = 0;
 
-// Get all color squares
-const colorSquares = document.querySelectorAll('.content-color .a-1');
-
-// Get the "concat" element
-const concatElement = document.getElementById('concat');
-
-// Get the "img-2" element
-const imgElement = document.querySelector('.img-2');
-
-// Function to change the background gradient
-function changeBackground(color) {
-    concatElement.style.background = ` ${color}`;
-}
-
-// Function to show the img and change background
-function showImageAndChangeBackground(color) {
-    imgElement.style.display = 'block'; // Show the img
-    changeBackground(color);
-
+function updateProduct(title, description, imageSrc, color) {
+    productTitle.textContent = title;
+    productDescription.textContent = description;
+    productImage1.classList.add('product-image'); // Add the class for transition
+    productImage2.classList.add('product-image'); // Add the class for transition
+    container.style.background = color;
+    
     setTimeout(() => {
-        imgElement.style.display = 'none'; // Hide the img after a delay
-    }, 4000); // Adjust the duration as needed (in milliseconds)
+        productImage1.src = imageSrc;
+        // productImage2.src = imageSrc;
+        container.style.background = color;
+    }, 500);
 }
 
-// Add click event listeners to each color square
-colorSquares.forEach(square => {
-    square.addEventListener('click', () => {
-        const bgColor = square.getAttribute('data-color');
-        showImageAndChangeBackground(bgColor);
-    });
-});
+
+function updateProduct(title, description, imageSrc, color) {
+    productTitle.textContent = title;
+    productDescription.textContent = description;
+    productImage1.classList.add('product-image'); // Add the class for transition
+    productImage2.classList.add('product-image'); // Add the class for transition
+    container.style.background = color;
+    
+    setTimeout(() => {
+        productImage1.src = imageSrc;
+        // productImage2.src = imageSrc;
+        container.style.background = color;
+
+        // Remove the .product-image class after a short delay
+        setTimeout(() => {
+            productImage1.classList.remove('product-image');
+            productImage2.classList.remove('product-image');
+        }, 100); // Adjust the delay as needed
+    }, 500); 
+}
+
+function clickNextImage() {
+    if (clickIndex < icons.length) {
+        icons[clickIndex].click();
+        clickIndex += 1;
+    } else {
+        clickIndex = 0;
+    }
+}
+
+// Set an interval to simulate clicks on every image
+let interval = setInterval(clickNextImage, 4000); // 2000 milliseconds = 2 seconds
+
+
+
+
+
+
+
+
+
+
+
 
 
